e41556ef7d3b7bfec2bfe055ab971b3abb7a2b15
陈桢梁 <chenzhenliang@xxpie.com>
2022-03-03 22:04:56 +0800

    初始结构搭建

--- main/main.go
+++ main/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
 
+var configMap = map[string]string{
+	"dev":  "config/dev.toml",
+	"dep":  "config/dep.toml",
+	"test": "config/test.toml",
+}
+
+func main() {
+	var env string
+	flag.StringVar(&env, "env", "", "dev")
+	flag.Parse()
+	g.Cfg().SetFileName(configMap[env])
+	s := g.Server()
+	s.BindHandler("GET:/get", func(r *ghttp.Request) {
+		r.Response.Writefln("success")
+	})
+	s.Run()
 }
